6aaf3654af66821f3ec9cabe5bc7b51820f78b69
lokeshpatidar14 <144682633+lokeshpatidar14@users.noreply.github.com>
2024-01-11 03:25:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <p>Expense Items</p>
+
+      <p>Food Rs 10</p>
+
+      <p>Petrol Rs 100</p>
+
+      <p>Movies Rs 200</p>
+    </div>
+  );
+}
+
+export default App;

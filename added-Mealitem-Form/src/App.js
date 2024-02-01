a0d8c7f66dd410003041151171c7ea24c036064c
lokeshpatidar14 <144682633+lokeshpatidar14@users.noreply.github.com>
2024-02-02 04:03:20 +0530

    added form

---
+++ added-Mealitem-Form/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Design/Header";
+
+import MealItems from "./components/meal/MealItems";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <MealItems></MealItems>
+    </>
+  );
+}
+
+export default App;
